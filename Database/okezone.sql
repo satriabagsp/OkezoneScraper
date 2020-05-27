bce3688cd61eb22437ca19ab243f9b8771a71734
Satria Bagus Panuntun <satriabgsp22@gmail.com>
2020-05-27 18:34:06 +0700

    Add files via upload
    Database file

---
+++ Database/okezone.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 27 Mei 2020 pada 13.29
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `okezone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `okezone`
+--
+
+CREATE TABLE `okezone` (
+  `sumber` varchar(100) DEFAULT NULL,
+  `keyword` varchar(100) DEFAULT NULL,
+  `url` varchar(1000) DEFAULT NULL,
+  `tanggal` varchar(150) DEFAULT NULL,
+  `judul` mediumtext,
+  `konten` mediumtext
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
